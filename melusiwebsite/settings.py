bdc7f8fbfae8ee9d3b283ac4ac7079fbb53ede44
Tumbry-10 <nallennash10@gmail.com>
2022-09-05 16:46:53 +0200

    Commit

--- melusiwebsite/settings.py
+++ melusiwebsite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ij+z@s(7eb-^$mx@*p7i_04ey95c$+gky#h^tj&w_s)0@$g*i='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','cadtech.herokuapp.com/']
 
